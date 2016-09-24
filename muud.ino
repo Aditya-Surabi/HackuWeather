9b740eb7f5027c51ee1d5d48d677e77803d7912a
satchelfrench <frenchsatchel@gmail.com>
2016-09-24 13:38:01 -0400

    added api call via webhook, store data in string

--- muud.ino
+++ muud.ino
@@ -1,14 +1,23 @@
+
 void setup(){
   Serial.begin(115200); //baud rate
-
   //subscribe to a webhook and lookout for new data
-  Particle.subscribe("hook-response/get_weather", gotWeatherData, MY_DEVICES);
-
-
+  Particle.subscribe("hook-response/get_weather", weatherHandler, MY_DEVICES);
 }
 
 void loop(){
 
+    Serial.println("Requesting Accuweather Data..");
     Particle.publish("get_weather");
-  
+
+    delay(60000);
+
+}
+
+void weatherHandler(const char *name, const char *data) {
+
+    String str = String(data);
+    Serial.println(str);
+    
+
 }
