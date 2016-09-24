e55327537372af6973f449151e4cdf18a1d26322
System Administrator <root@SATCHELs-MacBook-Pro.local>
2016-09-24 04:38:06 -0400

    added webhook

--- muud.ino
+++ muud.ino
@@ -1,7 +1,14 @@
 void setup(){
-  
+  Serial.begin(115200); //baud rate
+
+  //subscribe to a webhook and lookout for new data
+  Particle.subscribe("hook-response/get_weather", gotWeatherData, MY_DEVICES);
+
+
 }
 
 void loop(){
 
+    Particle.publish("get_weather");
+  
 }
