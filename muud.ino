0d97d321c1f21ffa472b8453b3c610392d7db6a1
satchelfrench <frenchsatchel@gmail.com>
2016-09-24 15:01:11 -0400

    added timer, webhook works via responsetemplate

--- muud.ino
+++ muud.ino
@@ -1,23 +1,30 @@
-
 void setup(){
   Serial.begin(115200); //baud rate
+
   //subscribe to a webhook and lookout for new data
   Particle.subscribe("hook-response/get_weather", weatherHandler, MY_DEVICES);
 }
 
 void loop(){
 
-    Serial.println("Requesting Accuweather Data..");
-    Particle.publish("get_weather");
+    // hold up, wait a minute lemme get some json in it.
+    for(int i=10; i >= 0; i--){
+      Serial.print("Seconds until data request: ");
+      Serial.print(i);
+      Serial.println();
+      delay(1000);
+    }
 
-    delay(60000);
+    Serial.println("Requesting Accuweather Data..");
+    Particle.publish("get_weather"); //publish webhook
+
+    delay(3000); // pls wait
 
 }
 
 void weatherHandler(const char *name, const char *data) {
 
-    String str = String(data);
-    Serial.println(str);
-    
+  String str = String(data);
+  Serial.println(str);
 
 }
