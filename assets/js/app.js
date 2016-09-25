0af9facdc772511772a524eac66347442d76ead2
satchelfrench <frenchsatchel@gmail.com>
2016-09-24 23:39:39 -0400

    dynamic jquery html + 8tracks player

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	$.getJSON("http://apidev.accuweather.com/currentconditions/v1/55488.json?apikey=HackuWeather2016", function(weatherData){ //don't bother the apikey is expired
 		var weatherIcon = weatherData[0].WeatherIcon;
 		console.log(weatherIcon);
+		console.log("");
 			}); // close weatherdata function
 
 	// write weather data to html	
@@ -17,14 +18,14 @@ $(document).ready(function(){
 	$.getJSON("https://api.spotify.com/v1/search?query=jazz&offset=30&limit=10&type=playlist", function(object){
 		var link = object["playlists"].href;
 		console.log(link);
-		console.log("hi");
+		console.log("shout out to the homies in da console!");
 
 		}); //close audio function
 
 	// write audio data to html	
 	$("#audio-player").append("\
-	<b>audio</b>"
-	);
+	<iframe class='sticky-bottom' src='https://8tracks.com/mixes/672112/player_v3_universal/' width='300' height='250' style='border: 0 none;''></iframe>\
+	");
 
 }); //close function main
 
--- assets/js/audio.js
+++
@@ -1,5 +0,0 @@
-$(document).ready($.getJSON("https://api.spotify.com/v1/search?query=jazz&offset=30&limit=10&type=playlist", function(object){
-	var link = object["playlists"].href;
-	console.log(link);
-	console.log("hi");
-}));
